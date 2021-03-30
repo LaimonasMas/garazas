2474214298c4204b27b52b630e08fe16a5a9bcc6
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-30 15:39:00 +0300

    Truck filter and sort ok

--- resources/views/truck/index.blade.php
+++ resources/views/truck/index.blade.php
@@ -5,7 +5,38 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Truck list</div>
+                <div class="card-header">
+
+                    <h2>Truck list</h2>
+                    <div class="make-inline">
+                        <form action="{{route('truck.index')}}" method="get" class="make-inline">
+                            <div class="form-group make-inline">
+                                <label>Mechanic: </label>
+                                <select class="form-control" name="mechanic_id">
+                                    <option value="0" @if($filterBy==0) selected @endif>All Mechanics</option>
+                                    @foreach ($mechanics as $mechanic)
+                                    <option value="{{$mechanic->id}}" @if($filterBy==$mechanic->id) selected @endif>
+                                        {{$mechanic->name}} {{$mechanic->surname}}
+                                    </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <label class="form-check-label">Sort by title:</label>
+                            <label class="form-check-label" for="sortASC">ASC</label>
+                            <div class="form-group make-inline column">
+                                <input type="radio" class="form-check-input" name="sort" value="asc" id="sortASC" @if($sortBy=='asc' ) checked @endif>
+                            </div>
+                            <label class="form-check-label" for="sortDESC">DESC</label>
+                            <div class="form-group make-inline column">
+                                <input type="radio" class="form-check-input" name="sort" value="desc" id="sortDESC" @if($sortBy=='desc' ) checked @endif>
+                            </div>
+                            <button type="submit" class="btn btn-outline-success btn-sm">Filter</button>
+                        </form>
+
+                        <a href="{{route('truck.index')}}" class="btn btn-outline-secondary btn-sm">Clear</a>
+                    </div>
+
+                </div>
                 <div class="card-body">
 
                     @foreach ($trucks as $truck)
